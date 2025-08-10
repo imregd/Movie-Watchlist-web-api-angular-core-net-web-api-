ff7d7e2cabf90b434788753a34553644f7753f09
aleks <aleks@DESKTOP-G6OPQHT>
2025-08-10 02:59:34 +0100

    Save changes to controller branch

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Movie_Watchlist_web_api__angular___core_net_web_api_.DB_Constructor;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DB_Constructor>(options =>
+    options.UseSqlite("Data source=MovieWatchlist.db"));
 
 var app = builder.Build();
 
