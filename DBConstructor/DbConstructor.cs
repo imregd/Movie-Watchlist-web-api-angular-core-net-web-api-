5eb82a101d0c13cd28d83176e6544d9afbf52eb0
aleks <aleks@DESKTOP-G6OPQHT>
2025-08-10 02:39:11 +0100

    Add project files.

---
+++ DBConstructor/DbConstructor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie_Watchlist_web_api__angular___core_net_web_api_.DBModel;
+
+namespace Movie_Watchlist_web_api__angular___core_net_web_api_.DB_Constructor
+{
+    public class DB_Constructor : DbContext
+    {
+        public DB_Constructor(DbContextOptions<DB_Constructor> options) : base(options)
+        {
+        }
+        public DbSet<> UserMovies { get; set; }
+    }
---
+++ DBModel/DbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Movie_Watchlist_web_api__angular___core_net_web_api_
+{
+    public class UserMovies
+    {
+        public int id { get; set; }
+        public string MovieName { get; set; }
+        public string MovieWatched { get; set; }
+        public string MovieRating { get; set; }
+    }
+}
