5fbb543bf15c051b26b5c740024f273976abf85f
aleks <aleks@DESKTOP-G6OPQHT>
2025-08-18 03:06:11 +0100

    WORKING and stable beginner for angular, it uses inline template which im familiar with

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
-import { Header } from './component/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, Home],
+  template: `
+    <h1>Welcome to {{ title() }}!</h1>
+    <app-home></app-home>
+    <router-outlet />
+  `,
+  styles: [],
 })
 export class App {
   protected readonly title = signal('client');
--- client/src/app/home/home.ts
+++ client/src/app/home/home.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.scss'
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styles: ``
 })
 export class Home {
 
